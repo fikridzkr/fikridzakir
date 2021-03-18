594bc982f6cfb891b98976d97c45582d2b629b9d
fikridzkr <fikridzakir179@gmail.com>
2021-03-18 11:54:51 +0700

    Setup Environmnet

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
